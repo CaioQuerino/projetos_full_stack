a5b0a0acbca5296c8eee3eeeb1de4a7ed53ced0d
Caio Querino <caioquerino04@gmail.com>
2025-08-04 23:12:08 -0300

    metodos da class TaskController disponível nas rotas

---
+++ nivel_iniciante/lista_de_tarefas/backend/src/controllers/task.controller.js
@@ -0,0 +1,24 @@
+import { schemaTask } from "../models/task.models.js";
+
+export class TaskController {
+    async create(req, res) {
+        return {}
+    }
+
+    async read(req, res) {
+        return {}
+    }
+
+    async readById(req, res) {
+        return {}
+    }
+
+    async update(req, res) {
+        return {}
+    }
+
+    async delete(req, res) {
+        return {}
+    }
+
+}
--- nivel_iniciante/lista_de_tarefas/backend/src/routes/task.routes.js
+++ nivel_iniciante/lista_de_tarefas/backend/src/routes/task.routes.js
@@ -1,11 +1,13 @@
 import  { Router } from 'express'
+import { TaskController } from '../controllers/task.controller.js'
 
+const taskController = new TaskController() 
 const router = Router()
 
-router.post('/task/', (req, res) => create(req, res))
-router.get('/tasks/', (req, res) => Read(req, res))
-router.get('/task/:id', (req, res) => ReadById(req, res))
-router.put('/task/:id', (req, res) => update(req, res))
-router.delete('/task:id/', (req, res) => delete(req, res))
+router.post('/task/', (req, res) => taskController.create(req, res))
+router.get('/tasks/', (req, res) => taskController.read(req, res))
+router.get('/task/:id', (req, res) => taskController.readById(req, res))
+router.put('/task/:id', (req, res) => taskController.update(req, res))
+router.delete('/task:id/', (req, res) => taskController.delete(req, res))
 
 export { router }
