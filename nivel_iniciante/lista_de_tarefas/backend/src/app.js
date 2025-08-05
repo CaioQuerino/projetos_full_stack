6409d9da4011db749d8eb61d15f5fb2f2d1c1326
Caio Querino <caioquerino04@gmail.com>
2025-08-04 22:35:01 -0300

    Adicionando uma função para rodar o servidor

--- nivel_iniciante/lista_de_tarefas/backend/src/app.js
+++ nivel_iniciante/lista_de_tarefas/backend/src/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
-import { configureMiddlewares } from './middlewares/app.middlewares';
+import { configureMiddlewares } from './middlewares/app.middlewares.js';
 
 const app = express()
 
 configureMiddlewares(app)
+
+export const Server = () => {
+    app.listen(3333, () => {
+        console.log('Servidor rodando na porta 3333')
+    })
+}
